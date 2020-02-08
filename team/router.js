e879d9ec20b17ddc91ff9412823b3b8be31785a2
Jitgitbit <rowowolf@gmail.com>
2020-02-08 03:07:02 +0100

    typo

--- team/router.js
+++ team/router.js
@@ -14,7 +14,7 @@ router.get('/teams', (req, res, next) => {
 router.get('/teams/:id', (req, res, next) => {
   Team.findByPk(req.params.id) // beware: if you want to define a const with req.params.id you have to parseInt(), because Url returns a string !!
     .then(team => {
-      res.json(team); //send for strings, json for data. it works both but it's better practice!
+      res.json(team); //.send for strings, .json for data. it works both but it's better practice!
     })
     .catch(err => next(err))
 });
